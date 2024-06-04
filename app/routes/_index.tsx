dfc94e18e9a844f920ea8ab43ec642916ae48fc8
Enza <e.eziz5@yandex.ru>
2024-06-04 17:39:01 +0500

    Remix Starter

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "New Remix App" },
+  ];
+};
+
+export default function RootPage() {
+  return (
+    <div
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
+      className="flex flex-col text-sm min-h-screen w-full"
+    ></div>
+  );
+}
