06d88a652fbdd6d1a87c892f9afcc7591a14af15
Enza <e.eziz5@yandex.ru>
2024-08-31 12:16:41 +0500

    Sum

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="flex flex-col min-h-screen w-full">
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,11 +9,8 @@ export const meta: MetaFunction = () => {
 
 export default function RootPage() {
   return (
-    <div
-      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
-      className="flex flex-col text-sm min-h-screen w-full"
-    >
-      <p>Hello REMIX</p>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <p className="font-mono">Hello Remix</p>
     </div>
   );
 }
